7546f0b5177c98afb13ee32490175bc068745be9
coba-yu <yuu.meiji.gkb@gmail.com>
2023-05-09 01:16:57 +0900

    add FreeeInvoice

---
+++ app/infrastructure/freee_invoice.go
@@ -0,0 +1,63 @@
+package infrastructure
+
+import (
+	"fmt"
+	"github.com/colere-inc/seen-api/app/common/config"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+const freeeInvoiceApiEndpointUrl = "https://api.freee.co.jp/iv/invoices"
+
+type FreeeInvoice struct {
+	Client      *http.Client
+	CompanyId   string
+	AccessToken string
+}
+
+func NewFreeeInvoice() *FreeeInvoice {
+	return &FreeeInvoice{
+		Client:      &http.Client{},
+		CompanyId:   config.FreeeCompanyId,
+		AccessToken: config.FreeeAccessToken,
+	}
+}
+
+func (fa *FreeeInvoice) Do(method string, path string, values url.Values, body io.Reader) Response {
+	// prepare request
+	requestUrl := freeeInvoiceApiEndpointUrl + path
+	if values != nil {
+		requestUrl = fmt.Sprintf("%s?%s", requestUrl, values.Encode())
+	}
+	req, err := http.NewRequest(method, requestUrl, body)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("accept", "application/json")
+	if method == http.MethodPost {
+		req.Header.Add("Content-Type", "application/json")
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", fa.AccessToken))
+
+	// request
+	client := fa.Client
+	res, err := client.Do(req)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(res.Body)
+
+	// convert to response
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	return Response{StatusCode: res.StatusCode, ResBody: resBody}
+}
